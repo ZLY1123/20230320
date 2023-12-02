2fd8b52a389984396410222f7e8a00ebc9c33b43
ZLY1123 <15146193609@163.com>
2023-12-02 15:46:34 +0800

    首次的代码提交

---
+++ files.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	printf("vcdfvsdfv"./n);
+
+	printf("vcdfvsdfv"./n);
+	printf("vcdfvsdfv"./n);
+	printf("vcdfvsdfv"./n);
+	printf("vcdfvsdfv"./n);
+	printf("vcdfvsdfv"./n);
+	printf("vcdfvsdfv"./n);
+}
